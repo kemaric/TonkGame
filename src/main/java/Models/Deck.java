4db0c9f7c01b455fb88a87d7f6686f25635f8f48
Kemari Legg <kemaric@gmail.com>
2016-10-07 15:16:11 -0400

    initial commit of Deck's properties

--- src/main/java/Models/Deck.java
+++ src/main/java/Models/Deck.java
@@ -1,7 +1,27 @@
 package Models;
 
+import java.util.ArrayList;
+
 /**
  * Created by Kemari on 10/7/2016.
  */
 public class Deck {
+    private ArrayList<Card> cards;
+
+
+    public Deck(){
+        cards = new ArrayList<Card>();
+        for (int i = 1 ; i <= 52 ; i++){
+            Card tmp = new Card();
+            cards.add(tmp);
+        }
+    }
+
+    public void shuffle(){
+
+    }
+
+    public Card drawFromDeck(){
+        return this.cards.get(0);
+    }
 }
